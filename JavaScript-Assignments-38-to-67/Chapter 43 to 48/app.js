55819d77571f3ff3759d378e0d5a5aed861c218b
Ahsan Raees <52628348+Ahsan4-coder@users.noreply.github.com>
2020-07-23 21:04:37 +0500

    Add files via upload

---
+++ JavaScript-Assignments-38-to-67/Chapter 43 to 48/app.js
@@ -0,0 +1,38 @@
+// Chapters 43 - 48 
+//  Task 01
+function clickMe() {
+    alert('Link Clicked');
+}
+
+//  Task 02
+function mobilePurchase() {
+    alert('Thanks for purchasing mobile from us');
+}
+
+// Task 03
+function deleteRow(e) {
+    e.parentNode.parentNode.remove();
+}
+
+// Task 04
+function changeImage(e) {
+    e.src = 'mobile02.jpg';
+}
+
+function revertImage(e) {
+    e.src = 'mobile01.jpg';
+}
+
+// Task 05
+var increaseBtn = document.getElementById('increase');
+var decreaseBtn = document.getElementById('decrease');
+var display_value = document.getElementById('display');
+var value_count = 0;
+increaseBtn.addEventListener('click', () => {
+    value_count++;
+    display_value.innerText = value_count;
+});
+decreaseBtn.addEventListener('click', () => {
+    value_count--;
+    display_value.innerText = value_count;
+});
